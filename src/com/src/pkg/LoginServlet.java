f97642ac26be1262b86ea62120bd8e66c4c89fce
Ben Perry <bap63@cornell.edu>
2012-11-16 21:39:47 -0500

    Login Updated
    Added error messages and redirects

--- src/com/src/pkg/LoginServlet.java
+++ src/com/src/pkg/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.src.pkg;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +13,9 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
+	boolean badPassword = false;
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -27,7 +30,47 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		//Redirect if already logged in
+		HttpSession userSession = request.getSession();
+		try
+		{
+			String loggedIn = userSession.getAttribute("loggedIn").toString();
+			response.sendRedirect("index.jsp");
+		}
+		catch(Exception e){}
+		
+		//Sets the redirect header
+		String redirect = request.getParameter("redirect");
+		if(redirect == null){
+			redirect = "index.jsp";
+		}
+		
+		//Determines if previous attempt was invalid
+		String loginError = " ";
+		if(badPassword){
+			loginError = "Invalid Username or Password";
+		}
+		
+		//Print login for HTMl code
+		String html = ""
+		+"<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN' 'http://www.w3.org/TR/html4/loose.dtd'>"
+		+"<html><head><meta http-equiv='Content-Type' content='text/html; charset=ISO-8859-1'>"
+		+"	<script src='http://code.jquery.com/jquery-latest.js'></script>"
+		+"	<title>Please Login to Continue</title>"
+		+"</head><body>"
+		+"	<form action='Login' method='post'>"
+		+"	<table id='passwordTable'>"
+		+"		<tr><td>Email</td><td> <input name='email' type='text' method='post'/> </td></tr>"
+		+"		<tr><td>Password </td><td> <input name='password' size=15 type='password' /></td></tr>"
+		+"	</table>"
+		+ 	"<p id='loginError'>"+loginError+"</p>"
+		+"	<input type='hidden' name='redirect' value='"+redirect+"'/>"
+		+"	<input type='submit' value='Login' />"
+		+"</form></body></html>";
+		out.print(html); 
+		
+
 	}
 
 	/**
@@ -39,16 +82,23 @@ public class LoginServlet extends HttpServlet {
 		if(redirect == null){
 			redirect = "index.jsp";
 		}
+		String loggedIn = request.getParameter("loggedIn");
+		if(loggedIn!=null)
+		{
+			response.sendRedirect(redirect);
+		}
+		
 		String email = request.getParameter("email");  
 		String password = request.getParameter("password");  
 		boolean auth = Security.login(email, password);
 		if(auth){
-			response.sendRedirect(redirect);
 			HttpSession userSession = request.getSession();
 			userSession.setAttribute("loggedIn", email);
+			response.sendRedirect(redirect);
 		}
 		else{
-			response.sendRedirect("login.jsp");
+			badPassword = true;
+			response.sendRedirect("Login");
 		}
 
 	}
