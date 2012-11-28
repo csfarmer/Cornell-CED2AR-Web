58aa72c7691ea09462f434240290581f336be6bd
Justin <jwb279@cornell.edu>
2012-11-27 20:54:59 -0500

    Advanced search fixed

--- WebContent/scripts/advancedSearch.js
+++ WebContent/scripts/advancedSearch.js
@@ -1,12 +1,14 @@
 jQuery(function(){
-
+	
 	// Add new row of form fields dynamically for advanced search
-	var rowCount = $("#advanced_table tr").length - 1; // Used to determine the ID of the current last text input field
+	var rowCount = $("#advanced_table tr").length - 2; // Used to determine the ID of the current last text input field
 	var selectRow = "input[name='advanced_search"+rowCount+"']"; // The last row that isn't submit
 	var idRowCount = rowCount + 1; // Used for entering the name and id of the possible new last row
+	$("#advanced_table select[name='searchBool"+rowCount+"']").hide();
 	// Adds a listener to the last text input box
 	function addRow() {
 		$("#advanced_table").on("keypress", selectRow, function() {
+			$("#advanced_table select[name='searchBool"+ rowCount +"']").show();
 			$("#advanced_table tr:last").before('<tr> \
 					<td> \
 					<select name="searchParam'+idRowCount+'"> \
@@ -31,9 +33,10 @@ jQuery(function(){
 
 			$("#advanced_table").off("keypress", selectRow); // Make it so the old input box won't add new rows
 			// Update the variables with the new length
-			rowCount = $("#advanced_table tr").length - 1;
+			rowCount = $("#advanced_table tr").length - 2;
 			selectRow = "input[name='advanced_search"+rowCount+"']";
 			idRowCount = rowCount + 1;
+			$("#advanced_table select[name='searchBool"+rowCount+"']").hide();
 			addRow(); // Add a listener for the new last row
 		});
 	}
